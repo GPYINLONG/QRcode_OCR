56755b3dfa9d87aed116687942cb15e714610d24
GPYINLONG <1434299501@qq.com>
2022-09-25 00:21:57 +0800

    new

--- pre.py
+++ pre.py
@@ -33,6 +33,32 @@ def threshold(img, keyword='otsu', max_val=0, adaptive_method=cv2.ADAPTIVE_THRES
         print('Keyword Error!')
 
 
+# 将图片分为m×n块
+def divide(img, m, n):
+    h, w = img.shape[0], img.shape[1]
+    grid_h = int(h * 1.0 / m)
+    grid_w = int(w * 1.0 / n)
+
+    # 计算满足整除关系的高与宽
+    h = grid_h * m
+    w = grid_w * n
+
+    # 进行图像缩放
+    resized = cv2.resize(img, (w, h), cv2.INTER_LINEAR)
+    gx, gy = np.meshgrid(np.linspace(0, w, n + 1), np.linspace(0, h, m + 1))
+    gx = gx.astype(np.int)
+    gy = gy.astype(np.int)
+
+    divided = np.zeros([m, n, grid_h, grid_w, 3], np.uint8)  # 五维张量
+
+    for i in range(m):
+        for j in range(n):
+            divided[i, j, ...] = resized[gy[i, j]:gy[i + 1, j + 1],
+                                         gx[i, j]:gx[i + 1, j + 1], :]
+
+    return divided
+
+
 if __name__ == '__main__':
     img = cv2.imread('./QRcode/1.jpeg')
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
